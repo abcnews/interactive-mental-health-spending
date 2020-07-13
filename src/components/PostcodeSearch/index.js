c2a030ae12365c02fa56318ffd83bbce21ec2cec
Joshua Byrd <phocks@gmail.com>
2020-07-13 14:48:25 +1000

    2 char min

--- src/components/PostcodeSearch/index.js
+++ src/components/PostcodeSearch/index.js
@@ -19,10 +19,14 @@ export default props => {
   // Fires when user sets postcode
   const handleChange = option => {
     console.log(option);
-    props.setUserPostcode(option.value)
+    props.setUserPostcode(option.value);
   };
 
   const filterPostcodes = inputValue => {
+    // Only search if user enters at least 2 characters
+    if (inputValue.length < 2) return [];
+
+    // Otherwise filter the postcodes and returns
     return options.filter(i =>
       i.label.toLowerCase().includes(inputValue.toLowerCase())
     );
