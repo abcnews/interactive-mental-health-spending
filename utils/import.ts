f54d1205636c604f2a75f66d52ce9ef4b5b400d0
Joshua Byrd <phocks@gmail.com>
2020-07-14 14:41:26 +1000

    Hmm

--- utils/import.ts
+++ utils/import.ts
@@ -1,9 +1,8 @@
+// @deno-types="./import.d.ts"
 import Papa from "https://jspm.dev/papaparse";
-
 const data = Deno.readTextFileSync("./mental-health-spending.csv");
 
 const onParseComplete = (results: any) => {
-  console.log(results.data);
   for (const entry of results.data) {
     console.log(entry)
   }
