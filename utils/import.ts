43072f6862eaddffdc91e431fceffd1be31b94e2
Joshua Byrd <phocks@gmail.com>
2020-07-14 16:50:10 +1000

    Finally fix annoying vs code error

--- utils/import.ts
+++ utils/import.ts
@@ -1,12 +1,15 @@
-/** @type {import('./import).Papa} */
-import Papa from "https://jspm.dev/papaparse";
+import PapaParse from "https://jspm.dev/papaparse";
+// Because we're using a pre es6 module it doesn't have typing
+const Papa: any = PapaParse;
+
+// Import data file
 const data = Deno.readTextFileSync("./mental-health-spending.csv");
 
 const onParseComplete = (results: any) => {
   for (const entry of results.data) {
-    console.log(entry)
+    console.log(entry);
   }
-}
+};
 
 Papa.parse(data, {
   complete: onParseComplete,
