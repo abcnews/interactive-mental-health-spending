82998844ad83296cc60894cd54f58fbed1cfbe3f
Joshua Byrd <phocks@gmail.com>
2020-07-15 11:33:23 +1000

    Docs

--- utils/import.ts
+++ utils/import.ts
@@ -1,5 +1,7 @@
 import PapaParse from "https://jspm.dev/papaparse";
+
 // Because we're using a pre es6 module it doesn't have typing
+// And I can't figure out how to import types properly... yet
 const Papa: any = PapaParse;
 
 // Import data file
