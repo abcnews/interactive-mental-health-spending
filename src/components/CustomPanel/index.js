ae7f68f52277db3ca8c13a6467fb576d8b8250ed
Joshua Byrd <phocks@gmail.com>
2020-08-04 14:22:50 +1000

    Custom panel

---
+++ src/components/CustomPanel/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.scss';
+
+export default props => {
+  return (
+    <div className={styles.root}>
+      Find me in <strong>src/components/CustomPanel/index.js</strong>
+    </div>
+  );
+}
