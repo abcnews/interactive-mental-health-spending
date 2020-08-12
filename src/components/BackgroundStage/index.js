887773c7a76cf4a2e485fc6f3dd10d32fe3c1dd8
Joshua Byrd <phocks@gmail.com>
2020-08-12 16:31:56 +1000

    A resizing axis!

--- src/components/BackgroundStage/index.js
+++ src/components/BackgroundStage/index.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles.scss";
+import * as d3Selection from "d3-selection";
+
+const d3 = { ...d3Selection };
 
 import MultiChart from "../MultiChart";
 
 export default props => {
+  useEffect(() => {
+    // Override width to account for scroll bar
+    const root = d3.select("." + styles.root);
+    const scrollyStage = root.select(function() {
+      return this.parentNode;
+    });
+    scrollyStage.style("width", "100%");
+  });
+
   return (
     <div className={styles.root}>
       <MultiChart />
