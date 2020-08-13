c76cf6df974e6f39dc7a33f02947c1b9ae0c01f1
Joshua Byrd <phocks@gmail.com>
2020-08-13 15:18:16 +1000

    Margins etc

--- src/components/MultiChart/index.js
+++ src/components/MultiChart/index.js
@@ -28,47 +28,117 @@ const wineQuality = require("./wineQuality.json");
 // const xAxisHeight = 35;
 // const textHeight = 14;
 
-const margin = {top: 40, right: 40, bottom: 40, left: 40};
-
 let svg;
+let margin;
 let scaleX;
+let scaleY;
 let xAxis;
-let axisGroup;
+let yAxis;
+let xAxisGroup;
+let yAxisGroup;
 let dots;
+let width;
+let height;
 
 export default props => {
   const root = useRef();
   const windowSize = useWindowSize();
 
   const initComponent = () => {
+    margin = {
+      top: window.innerHeight * 0.2,
+      right: window.innerWidth * 0.1,
+      bottom: window.innerHeight * 0.1,
+      left: window.innerWidth * 0.1
+    };
+
     svg = d3.select(root.current);
 
-    const width = svg.node().getBoundingClientRect().width;
-    const height = window.innerHeight;
+    width = svg.node().getBoundingClientRect().width;
+    height = window.innerHeight;
 
     const data = [
-      { xfield: 2, yfield: 55 },
-      { xfield: 3, yfield: 66 }
+      { xfield: 4, yfield: 55 },
+      { xfield: 5, yfield: 66 },
+      { xfield: 5, yfield: 76 }
     ];
 
     scaleX = d3
       .scalePoint()
-      .domain([1, 2, 3, 4, 5]) // This is what is written on the Axis: from 0 to 100
+      .domain([
+        "start",
+        1,
+        "spacer2",
+        2,
+        "spacer3",
+        3,
+        "spacer4",
+        4,
+        "spacer5",
+        5,
+        "end"
+      ])
       .range([margin.left, width - margin.right]);
 
-    xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(scaleX))
-    
-    
-    
+    scaleY = d3
+      .scaleLinear()
+      .domain([100, 1]) // This is what is written on the Axis: from 0 to 100
+      .range([margin.top, height - margin.bottom]);
+
+    xAxis = g => {
+      return g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+        d3
+          .axisBottom(scaleX)
+          .tickFormat("")
+          // .ticks([8])
+          .tickValues([
+            "start",
+            "spacer2",
+            "spacer3",
+            "spacer4",
+            "spacer5",
+            "end"
+          ])
+        // .tickSize(-200)
+      );
+    };
+
+    // yAxis = g => {
+    //   return g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+    //     d3
+    //       .axisLeft(scaleX)
+    //       .tickFormat("")
+    //       .ticks(5)
+    //     // .tickSize(-200)
+    //   );
+    // };
+
+    yAxis = svg =>
+      svg
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(
+          d3.axisRight(scaleY).tickSize(width - margin.left - margin.right)
+          // .tickFormat(formatTick)
+        )
+        .call(g => g.select(".domain").remove())
+        .call(g =>
+          g
+            .selectAll(".tick line")
+            .attr("stroke-opacity", 0.5)
+            .attr("stroke-dasharray", "2,2")
+        )
+        .call(g =>
+          g
+            .selectAll(".tick text")
+            .attr("x", 4)
+            .attr("dy", -4)
+        );
+
     //d3.axisBottom(scaleX).tickSize(3);
 
     // Draw the axis
-    axisGroup = svg
-      .append("g")
-      // .attr("transform", "translate(0,200)") // This controls the vertical position of the Axis
-      .call(xAxis);
+    xAxisGroup = svg.append("g").call(xAxis);
+    yAxisGroup = svg.append("g").call(yAxis);
 
     svg.attr("width", width);
     svg.attr("height", height);
@@ -82,7 +152,7 @@ export default props => {
       .append("circle")
       .attr("fill", "#435699")
       .attr("cx", d => scaleX(d.xfield))
-      .attr("cy", d => d.yfield)
+      .attr("cy", d => scaleY(d.yfield))
       .attr("r", 4);
   };
 
@@ -90,24 +160,35 @@ export default props => {
     // Init layout effect after delay
     setTimeout(() => {
       initComponent();
-    }, 2000);
+    }, 500);
   }, []);
 
+  // Detect and handle window resize events
   useLayoutEffect(() => {
     if (!svg) return;
 
-    console.log("Resize detected...");
-    console.log(svg.node().getBoundingClientRect());
+    // Recalculate margins
+    margin = {
+      top: window.innerHeight * 0.2,
+      right: window.innerWidth * 0.1,
+      bottom: window.innerHeight * 0.1,
+      left: window.innerWidth * 0.1
+    };
 
-    const width = svg.node().getBoundingClientRect().width;
-    const height = svg.node().getBoundingClientRect().height;
+    // console.log("Resize detected...");
+    // console.log(svg.node().getBoundingClientRect());
 
-    scaleX.range([0 + margin, width - margin]);
+    width = svg.node().getBoundingClientRect().width;
+    height = window.innerHeight;
+
+    scaleX.range([margin.left, width - margin.right]);
+    scaleY.range([margin.top, height - margin.bottom]);
 
     svg.attr("width", width);
     svg.attr("height", height);
 
-    axisGroup.call(axis);
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
   }, [windowSize.width, windowSize.height]);
 
   // useEffect(() => {
