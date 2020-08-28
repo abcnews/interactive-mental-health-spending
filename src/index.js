54296d86db881d0ea5f85dac1eb1e4da03a817bb
Joshua Byrd <phocks@gmail.com>
2020-08-28 12:06:48 +1000

    Add extra scrollyteller

--- src/index.js
+++ src/index.js
@@ -14,28 +14,40 @@ const d3 = { ...d3Selection };
 import buildMountPoints from "./lib/buildMountPoints";
 import { addClass } from "./lib/classHelpers";
 
-buildMountPoints(["postcodesearch", "scrollystagemount"]);
+buildMountPoints(["postcodesearch", "scrollystagemount", "scrollystagemount2"]);
 
 // Make stage full width
 const stage = document.querySelector(".scrollystagemount");
 addClass(stage, "u-full");
 addClass(stage, "no-margin-collapse");
 
+const stage2 = document.querySelector(".scrollystagemount2");
+addClass(stage2, "u-full");
+addClass(stage2, "no-margin-collapse");
+
 const preInit = () => {
   // Handle stuff that we don't want to hot reload
+  // (but do want to wait for Odyssey)
   const heroEl = d3.select(".Header").insert("div", ":first-child");
   heroEl.classed("pre-header-hero", true);
 };
 
 const init = async () => {
+  // Let's mount our scrollytellers
   const scrollyData1 = loadScrollyteller(
     "one", // If set to eg. "one" use #scrollytellerNAMEone in CoreMedia
     "u-full", // Class to apply to mount point u-full makes it full width in Odyssey
     "mark" // Name of marker in CoreMedia eg. for "point" use #point default: #mark
   );
 
+  const scrollyData2 = loadScrollyteller(
+    "two", // If set to eg. "one" use #scrollytellerNAMEone in CoreMedia
+    "u-full", // Class to apply to mount point u-full makes it full width in Odyssey
+    "mark" // Name of marker in CoreMedia eg. for "point" use #point default: #mark
+  );
+
   render(
-    <App projectName={"Mental Health"} scrollyData={scrollyData1} />,
+    <App projectName={"Mental Health"} scrollyData1={scrollyData1} scrollyData2={scrollyData2} />,
     document.querySelector(".postcodesearch")
   );
 };
