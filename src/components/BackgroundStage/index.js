aa67d79b1ae6b0e412d52d01df0bd606ff9665e3
Joshua Byrd <phocks@gmail.com>
2020-09-15 11:41:08 +1000

    Fix 100% issue on multiple components

--- src/components/BackgroundStage/index.js
+++ src/components/BackgroundStage/index.js
@@ -1,23 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./styles.scss";
 import * as d3Selection from "d3-selection";
 
 const d3 = { ...d3Selection };
 
-export default props => {
+const BackgroundStage = (props) => {
+  const stageRef = useRef();
+
   useEffect(() => {
     // Override width to account for scroll bar
-    const root = d3.select("." + styles.root);
-    const scrollyStage = root.select(function() {
+    const root = d3.select(stageRef.current);
+    const scrollyStage = root.select(function () {
       return this.parentNode;
     });
+
     scrollyStage.style("width", "100%");
   });
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} ref={stageRef}>
       {props.children}
       {/* <MultiChart /> */}
     </div>
   );
 };
+
+export default BackgroundStage;
