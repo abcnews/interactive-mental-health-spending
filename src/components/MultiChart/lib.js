0618d49f61391a9e2b23971c2511c70cac452d2e
Joshua Byrd <phocks@gmail.com>
2020-09-24 13:57:01 +1000

    Highly distressed section pretty much done

--- src/components/MultiChart/lib.js
+++ src/components/MultiChart/lib.js
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 export const sortData = (data, sortKey) => {
   return data.sort((a, b) => a[sortKey] - b[sortKey]);
 };
@@ -67,3 +69,18 @@ export const generateAverageData = (data, groupName, valueKey) => {
   }
   return meanData;
 };
+
+// Hook
+export function usePrevious(value) {
+  // The ref object is a generic container whose current property is mutable ...
+  // ... and can hold any value, similar to an instance property on a class
+  const ref = useRef();
+
+  // Store current value in ref
+  useEffect(() => {
+    ref.current = value;
+  }, [value]); // Only re-run if value changes
+
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
+}
