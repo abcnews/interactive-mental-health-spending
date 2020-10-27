da469eb63406bf86eab383e7c61bcedb39d78a88
Joshua Byrd <phocks@gmail.com>
2020-10-27 14:21:31 +1000

    More data

--- utils/sa3-data/process.ts
+++ utils/sa3-data/process.ts
@@ -1,8 +1,7 @@
-const data = require("./original-data.json");
 const fs = require("fs");
 
-// console.log(data);
-
+// Allied mental health data
+const data = require("./clinicalPsychologist.json");
 const ourData = {};
 
 for (const d of data) {
@@ -24,4 +23,29 @@ for (const d of data) {
 
 console.log(ourData);
 
-fs.writeFileSync("./ourData.json", JSON.stringify(ourData));
+fs.writeFileSync("./clinicalPsychologistCombined.json", JSON.stringify(ourData));
+
+// Other allied mental health data
+// const otherData = require("./other-allied.json");
+// const otherOurData = {};
+
+// for (const d of data) {
+//   if (!otherOurData[d.code]) otherOurData[d.code] = {};
+//   otherOurData[d.code].name = d.name;
+
+//   if (d.measure === "Per cent of people who had the service (%)") {
+//     otherOurData[d.code]["percentOfPeople"] = d.value;
+//   }
+
+//   if (d.measure === "Services per 100 people") {
+//     otherOurData[d.code]["servicesPer100"] = d.value;
+//   }
+
+//   if (d.measure === "Medicare benefits per 100 people ($)") {
+//     otherOurData[d.code]["dollarsPer100"] = d.value;
+//   }
+// }
+
+// console.log(otherOurData);
+
+// fs.writeFileSync("./otherOurData.json", JSON.stringify(otherOurData));
