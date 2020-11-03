f798323ff90707d27d139e1ab76494a5b7e361b0
Joshua Byrd <phocks@gmail.com>
2020-11-03 13:28:11 +1000

    mounting inline chart

---
+++ src/components/AverageLineChart/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.scss';
+
+const AverageLineChart = () => {
+  return (
+    <div className={styles.root}>
+      Find me in <strong>src/components/AverageLineChart</strong>
+    </div>
+  );
+}
+
+export default AverageLineChart;
