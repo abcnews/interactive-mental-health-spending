f0e5d44f1de00e79bbdadf597300ffef06eafd36
Joshua Byrd <phocks@gmail.com>
2020-12-07 11:03:51 +1000

    Remoe consoles

--- src/components/ErrorBox/index.js
+++ src/components/ErrorBox/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import styles from './styles.css';
 
 export default props => {
-  useEffect(() => console.log(props.error), []);
+  useEffect(() => console.error(props.error), []);
 
   return <pre className={styles.root}>{`${String(props.error)}\n\n${props.error.stack}`}</pre>;
 };
